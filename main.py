4f4752ec22c2449cd017deb10c747788dffd6bbd
kateav0197 <kateav0197@gmail.com>
2025-03-23 15:57:29 -0500

    "Subir archivo main.py"

---
+++ main.py
@@ -0,0 +1,9 @@
+class LearningGit:
+    def __init__(self):
+        print("Learning Git")
+
+    def funcionXParaPrueba(self):
+        print("Función X para prueba")
+
+    def funcionTerminado(self):
+        print("Función terminado")
