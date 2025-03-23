f19d754067f7f187b65554b3160ed0bcc8c6321e
kateav0197 <kateav0197@gmail.com>
2025-03-23 16:44:48 -0500

    Math operations

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def sum(x,y):
+    return x + y
+
+def substraction(x, y):
+    return x-y
+
+def multiplication(x, y):
+    return x*y
+
+def division(x,y):
+    return x/y
+
+def power(x,y):
+    return x**y
+
+def square_root(x):
+    return x**0.5
