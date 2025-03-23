61ca0aa934b108786afef936ab97d4c1f17605fe
kateav0197 <kateav0197@gmail.com>
2025-03-23 16:11:42 -0500

    Eliminar línea

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ class LearningGit:
     def __init__(self):
         print("Learning Git")
 
-    def funcionXParaPrueba(self):
-        print("Función X para prueba")
-
     def funcionTerminado(self):
         print("Función terminado")
 
